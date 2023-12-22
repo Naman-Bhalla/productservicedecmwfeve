f4569a7ef993787de35e6e762ecac8925a121d9b
Yashasvi Galav <galavyashasvi23@gmail.com>
2023-12-23 00:10:53 +0530

    Implement get all products endpoint

--- src/main/java/com/scaler/productservicedecmwfeve/services/FakeStoreProductService.java
+++ src/main/java/com/scaler/productservicedecmwfeve/services/FakeStoreProductService.java
@@ -4,17 +4,42 @@ import com.scaler.productservicedecmwfeve.dtos.FakeStoreProductDto;
 import com.scaler.productservicedecmwfeve.models.Category;
 import com.scaler.productservicedecmwfeve.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @Service
 public class FakeStoreProductService implements ProductService {
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
     @Autowired
     public FakeStoreProductService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public Product getSingleProduct(Long id) {
+        FakeStoreProductDto productDto = restTemplate.getForObject(
+                "https://fakestoreapi.com/products/" + id,
+                FakeStoreProductDto.class
+        );
+
+        return productDto != null ? convertFakeStoreProductToProduct(productDto) : null;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        FakeStoreProductDto[] productDtos = restTemplate.getForObject(
+                "https://fakestoreapi.com/products",
+                FakeStoreProductDto[].class
+        );
+
+        return productDtos != null ? Arrays.stream(productDtos)
+                .map(this::convertFakeStoreProductToProduct)
+                .toList() : Collections.emptyList();
     }
 
     private Product convertFakeStoreProductToProduct(FakeStoreProductDto fakeStoreProduct) {
@@ -28,15 +53,5 @@ public class FakeStoreProductService implements ProductService {
         product.getCategory().setName(fakeStoreProduct.getCategory());
 
         return product;
-    }
-
-    @Override
-    public Product getSingleProduct(Long id) {
-        FakeStoreProductDto productDto = restTemplate.getForObject(
-                "https://fakestoreapi.com/products/" + id,
-                FakeStoreProductDto.class
-        );
-
-        return convertFakeStoreProductToProduct(productDto);
     }
 }
--- src/main/java/com/scaler/productservicedecmwfeve/services/ProductService.java
+++ src/main/java/com/scaler/productservicedecmwfeve/services/ProductService.java
@@ -2,7 +2,11 @@ package com.scaler.productservicedecmwfeve.services;
 
 import com.scaler.productservicedecmwfeve.models.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
     Product getSingleProduct(Long id);
+
+    List<Product> getAllProducts();
 }
